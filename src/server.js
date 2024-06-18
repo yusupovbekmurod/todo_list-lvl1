db5079c47323e8b6c2c9bddb38a1004d7cf8938f
bekmurod_yusupov <bekmurodyusupov81@gmail.com>
2024-06-18 11:23:49 +0500

    debug

--- src/server.js
+++ src/server.js
@@ -9,7 +9,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
 app.use("/todo", todoRouter);
+
 
 app.listen(PORT);
 console.log("server listening " + PORT);
