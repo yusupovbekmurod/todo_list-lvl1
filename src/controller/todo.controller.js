e14263dfd34c208e1c76de7aea799d8abc092b8e
bekmurod_yusupov <bekmurodyusupov81@gmail.com>
2024-08-15 11:24:03 +0500

    edit invoices

--- src/controller/todo.controller.js
+++ src/controller/todo.controller.js
@@ -1,89 +1,118 @@
 import todoList from "../models/todo.models.js";
 
 class todoContr {
-    async get(req, res) {
-        try {
-            const id = req.params?.id;
-            let data;
-            if (id) data = await todoList.select(id);
-            else if (Object.keys(req.query).length)
-                data = await todoList.select(null, req.query);
-            else data = await todoList.select();
-            return res.send({
-                status: 200,
-                data,
-                message: "todoList",
-            });
-        } catch (error) {
-            return res.status(404).json({
-                status: 404,
-                data: null,
-                message: error.message,
-            });
-        }
+  async get(req, res) {
+    try {
+      const id = req.params?.id;
+      let data;
+      if (id) data = await todoList.select(id);
+      else if (Object.keys(req.query).length)
+        data = await todoList.select(null, req.query);
+      else data = await todoList.select();
+      return res.send({
+        status: 200,
+        data,
+        message: "todoList",
+      });
+    } catch (error) {
+      return res.status(404).json({
+        status: 404,
+        data: null,
+        message: error.message,
+      });
     }
-    async post(req, res) {
-        try {
-            let { title, description } = req.body;
+  }
+  async post(req, res) {
+    try {
+      let {
+        user_id,
+        name,
+        price,
+        active,
+        street_address,
+        city,
+        post_code,
+        country,
+      } = req.body;
 
+      res.send({
+        status: 201,
+        data: await todoList.insert({
+          user_id: user_id,
+          name: name,
+          price: price,
+          active: active,
+          street_address: street_address,
+          city: city,
+          post_code: post_code,
+          country: country,
+        }),
+        message: "success",
+      });
+    } catch (error) {
+      return res.status(400).json({
+        status: 400,
+        data: null,
+        message: error.message,
+      });
+    }
+  }
+  async put(req, res) {
+    try {
+      const id = req.params?.id;
+      let data;
+      if (id) data = await todoList.select(id);
+      let {
+        user_id,
+        name,
+        price,
+        active,
+        street_address,
+        city,
+        post_code,
+        country,
+      } = req.body;
 
-            res.send({
-                status: 201,
-                data: await todoList.insert({
-                    title: title,
-                    description: description
-
-                }),
-                message: "success",
-            });
-        } catch (error) {
-            return res.status(400).json({
-                status: 400,
-                data: null,
-                message: error.message,
-            });
-        }
+      const obj = {
+        $set: {
+          user_id: user_id ? user_id : data.user_id,
+          name: name ? name : data.name,
+          price: price ? price : data.price,
+          active: active ? active : data.active,
+          street_address: street_address ? street_address : data.street_address,
+          city: city ? city : data.city,
+          post_code: post_code ? post_code : data.post_code,
+          country: country ? country : data.country,
+        },
+      };
+      return res.send({
+        status: 201,
+        data: await todoList.update({ _id: id }, obj),
+      });
+    } catch (error) {
+      return res.status(404).json({
+        status: 404,
+        data: null,
+        message: error.message,
+      });
     }
-    async put(req, res) {
-        try {
-            const id = req.params?.id;
-            let data;
-            if (id) data = await todoList.select(id);
-            let { title, description } = req.body;
-            const obj = {
-                $set: {
-                    title: title ? title : data.title,
-                    description: description ? description : data.description,
-                },
-            };
-            return res.send({
-                status: 201,
-                data: await todoList.update({ _id: id }, obj),
-            });
-        } catch (error) {
-            return res.status(404).json({
-                status: 404,
-                data: null,
-                message: error.message,
-            });
-        }
+  }
+  async delete(req, res) {
+    try {
+      const id = req.params?.id;
+      return res.send({
+        status: 201,
+        data: await todoList.delete(id),
+        message: "success",
+      });
+    } catch (error) {
+      return res.status(404).json({
+        status: 404,
+        data: null,
+        message: error.message,
+      });
     }
-    async delete(req, res) {
-        try {
-            const id = req.params?.id;
-            return res.send({
-                status: 201,
-                data: await todoList.delete(id),
-                message: "success",
-            });
-        } catch (error) {
-            return res.status(404).json({
-                status: 404,
-                data: null,
-                message: error.message,
-            });
-        }
-    }
+  }
 }
 
 export default new todoContr();
--- src/schemas/todo.schema.js
+++ src/schemas/todo.schema.js
@@ -1,25 +1,43 @@
 import { Schema, model, Types } from "mongoose";
 const TodoSchema = new Schema(
-    {
-
-        title: {
-            type: String,
-            required: true
-        },
-
-        description: {
-            type: String,
-            required: true
-        },
-
+  {
+    user_id: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: {
-            createdAt: "create_at",
-            updatedAt: "update_at",
-            deletedAt: "delete_at",
-        },
-    }
+
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: String,
+      required: true,
+    },
+    active: {
+      type: String,
+      required: true,
+    },
+    street_address: {
+      type: String,
+    },
+    city: {
+      type: String,
+    },
+    post_code: {
+      type: String,
+    },
+    country: {
+      type: String,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: "create_at",
+      updatedAt: "update_at",
+      deletedAt: "delete_at",
+    },
+  }
 );
 
 export default model("todo", TodoSchema);
